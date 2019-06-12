0462f61d2ae8b3c02f6e79e8e7dd5e6bd56d71fa
Jakub Śmietański <kuba.smietanski@gmail.com>
2019-06-12 02:42:23 +0200

    init

---
+++ AngularTutorial.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using AngularTutorial.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularTutorial.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
